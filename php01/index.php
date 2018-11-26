08fad779e6900e58538a6b3b3c20f0892b04a340
silesrap <laimonas.sk@gmail.com>
2018-11-26 22:39:06 +0200

    php01

---
+++ php01/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  $country = 'Lietuva';
+  $city = 'Vilnius';
+  $population = 574221;
+
+  echo "<ul><li>$country</li><li>$city</li><li>$population</li></ul>";
+
+
+ ?>
---
+++ php02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$country = 'Lietuva';
+$city = 'Vilnius';
+$population = 574221;
+
+echo $city . ' - Lietuvos sostinė<br>';
+echo "$city ir $country sparciai auga";
+
+?>
