4692c2223b3c6685b2e4f10f58ef8a1501734c5c
silesrap <laimonas.sk@gmail.com>
2018-11-29 19:43:02 +0200

    PHP11

---
+++ php11/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if (isset($_POST['submit']).$_POST['ilgis'] && $_POST['plotis']) {
+          echo ($_POST['ilgis'] * $_POST['plotis']);
+        }
+    ?>
+    <form class='' action='<?php $_PHP_SELF; ?>' method='POST'>
+      <input type='text' name='ilgis' placeholder='ilgis' value=''>
+      <input type='text' name='plotis' placeholder='plotis' value=''>
+      <input type="submit">
+    </form>
+  </body>
+</html>
